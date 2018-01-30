4b264b35bb9b8ff23589615bb3effd2cf903c6b0
Paul Gadi <paulgadI@gmail.com>
2018-01-30 19:16:31 +0800

    commented out conditionals to more easily run buyObject

--- contracts/FreeShop.sol
+++ contracts/FreeShop.sol
@@ -352,9 +352,10 @@ contract Shop is Ownable {
 
     }
 
-    function buyObject(address _beneficiary) onlyValidPurchase
-    onlyBuyer(_beneficiary)
-    onlyValidAddress(_beneficiary) public payable {
+    function buyObject(address _beneficiary) //  onlyValidPurchase
+    // onlyBuyer(_beneficiary)
+    // onlyValidAddress(_beneficiary)
+    public payable {
         uint numTokens = calculateTokens();
         shopSettings.fundsRaised = shopSettings.fundsRaised.add(msg.value);
         object.mint(_beneficiary, numTokens);
--- migrations/3_deploy_freeshop.js
+++ migrations/3_deploy_freeshop.js
@@ -1,5 +1,5 @@
 var FreeShop = artifacts.require("FreeShop");
 
 module.exports = function(deployer) {
-  deployer.deploy(FreeShop, "FreeShop", "FREE", Date.now() + (1 * 24 * 60 * 60 * 1000), {gas: 6721975});
+  deployer.deploy(FreeShop, "FreeShop", "FREE", Date.now() + (1 * 24 * 60 * 60 * 1000));
 };
